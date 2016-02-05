9d28eb11e8741cc34e7a9c01b71bec6df02082d1
Hay Kranen <huskyr@gmail.com>
2016-02-05 17:41:37 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var yaml = require('js-yaml');
+var marked = require('marked');
+
+const BLOCK_SEPERATOR = '---';
+
+function parse(text) {
+    var block, lines;
+    var blocks = [];
+    var TAG_OPEN = false;
+    var first = true;
+
+    text.split('\n').forEach((line) => {
+        if (line.trim().includes(BLOCK_SEPERATOR)) {
+            if (TAG_OPEN) {
+                block.meta = yaml.safeLoad(lines.join('\n'));
+                lines = [];
+            } else {
+                if (!first) {
+                    block.content = marked(lines.join('\n'));
+                    blocks.push(block);
+                } else {
+                    first = false;
+                }
+
+                lines = [];
+                block = {};
+            }
+
+            TAG_OPEN = !TAG_OPEN;
+        } else {
+            lines.push(line);
+        }
+    });
+
+    return blocks;
+}
+
+module.exports = {
+    parse : parse
+};
---
+++ readme.md
@@ -0,0 +1,24 @@
+# jux
+> YAML / Markdown format to write structured narratives
+
+## Install
+
+```
+$ npm install --save jux
+```
+
+## Usage
+
+```js
+const jux = require('jux');
+
+jux.parse('...');
+```
+
+## API
+
+### jux.parse(input)
+
+## License
+
+MIT © [Hay Kranen](http://haykranen.nl)
